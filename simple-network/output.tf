e06e540a4bec2ad809221ca019f67c5153491c8f
Denis <dsantos@Deniss-MacBook-Pro.local>
2023-10-05 11:19:25 -0700

    add-outputs

--- simple-network/output.tf
+++ simple-network/output.tf
@@ -2,6 +2,14 @@ output "public_subnet_id" {
   value = aws_subnet.snet_public_a[0].id
 }
 
+output "private_subnet_a_id" {
+  value = aws_subnet.snet_private_a[0].id
+}
+
+output "private_subnet_b_id" {
+  value = aws_subnet.snet_private_b[0].id
+}
+
 output "vpc_id" {
   value = aws_vpc.main.id
 }
